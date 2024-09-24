da71170dfb7f025305a3176292c725b924f3ce50
VikasMeshram2708 <meshramvikas744@gmail.com>
2024-09-24 20:00:36 +0530

    :sparkles: navbar created.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
+import Navbar from "@/components/Navbar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -58,6 +59,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Navbar />
         <Toaster />
         {children}
       </body>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,138 +1,79 @@
-"use client";
-
-import React, { useEffect, useState } from "react";
-import { LogOut, Menu, ShoppingCart } from "lucide-react";
-import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "./ui/button";
+import { Menu } from "lucide-react";
 import {
   Sheet,
   SheetContent,
+  SheetDescription,
   SheetHeader,
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { Input } from "@/components/ui/input";
-import { signOut, useSession } from "next-auth/react";
-import { useSelector } from "react-redux";
-import { RootState } from "@/app/store/store";
-import Link from "next/link";
-import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { links } from "@/app/seed/NavLInks";
+import { Input } from "./ui/input";
 
-export const Navbar = () => {
-  const { status } = useSession();
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const { products } = useSelector((state: RootState) => state.product);
-
-  useEffect(() => {
-    if (status === "authenticated") {
-      setIsAuthenticated(true);
-    }
-
-    if (status === "unauthenticated") {
-      setIsAuthenticated(false);
-    }
-  }, [status]);
-
-  const handleLogout = async () => {
-    await signOut();
-  };
-
+export default function Navbar() {
   return (
-    <nav className="text-white shadow-md">
-      <div className="container mx-auto px-4 py-3 flex justify-between gap-3 items-center">
-        <Link href="/" className="text-xl font-bold">
-          <Avatar>
-            <AvatarImage src="/nav-logo.png" />
-            <AvatarFallback>CN</AvatarFallback>
-          </Avatar>
-        </Link>
-
-        <div className="hidden md:flex w-full md:max-w-4xl lg:max-w-5xl mx-auto items-center space-x-4">
-          <Input placeholder="Search" />
+    <header className="px-4 py-2 w-full">
+      <nav className="container mx-auto flex items-center justify-between gap-3">
+        <div>
+          <Link href="/">
+            <Avatar>
+              <AvatarImage src="./nav-logo.png" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </Link>
         </div>
-
-        <div className="hidden md:flex items-center space-x-4">
-          {links.map((link) => (
-            <Link
-              key={link.label}
-              href={link.url}
-              className="hover:text-gray-300"
-            >
-              {link.label}
-            </Link>
-          ))}
-
-          <DropdownMenu>
-            <DropdownMenuTrigger className="relative" asChild>
-              <Button variant="ghost" size="icon">
-                <ShoppingCart className="h-5 w-5" />
-                {products?.length > 0 && (
-                  <span className="absolute top-0 right-0 bg-red-500 text-white text-xs rounded-full h-4 w-4 flex items-center justify-center">
-                    {products?.length}
-                  </span>
-                )}
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-              <DropdownMenuLabel>Cart</DropdownMenuLabel>
-              <DropdownMenuItem>View Cart</DropdownMenuItem>
-              <DropdownMenuItem>Checkout</DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-
-          {status === "loading" ? (
-            <span className="myLoader"></span>
-          ) : isAuthenticated ? (
-            <Button onClick={handleLogout} variant="destructive" size="sm">
-              <LogOut className="mr-2 h-4 w-4" />
-              Logout
-            </Button>
-          ) : (
-            <Button variant="secondary" size="sm">
-              Login
-            </Button>
-          )}
+        <div className="hidden lg:block w-full max-w-lg md:max-w-xl lg:max-w-4xl xl:max-w-6xl mx-auto">
+          <Input type="text" placeholder="Search" />
         </div>
-
+        <div className="hidden lg:flex gap-3">
+          <Button variant={"secondary"}>
+            <Link href="/login">Login</Link>
+          </Button>
+          <Button variant={"outline"}>
+            <Link href="/signup">Sign Up</Link>
+          </Button>
+        </div>
+        {/* Mobile Nav */}
         <Sheet>
-          <SheetTrigger asChild>
-            <Button variant="ghost" size="icon" className="md:hidden">
-              <Menu className="h-5 w-5" />
-            </Button>
+          <SheetTrigger className="lg:hidden">
+            <Menu />
           </SheetTrigger>
-          <SheetContent side="right" className="bg-gray-800 text-white">
+          <SheetContent className="dark">
             <SheetHeader>
-              <SheetTitle className="text-white">Menu</SheetTitle>
-            </SheetHeader>
-            <div className="mt-6 flex flex-col space-y-4">
-              {links.map((link) => (
-                <Link
-                  key={link.label}
-                  href={link.url}
-                  className="text-lg hover:text-gray-300"
-                >
-                  {link.label}
+              <SheetTitle>
+                <Link href="/">
+                  <Avatar>
+                    <AvatarImage src="./nav-logo.png" />
+                    <AvatarFallback>CN</AvatarFallback>
+                  </Avatar>
                 </Link>
-              ))}
-              {isAuthenticated ? (
-                <Button onClick={handleLogout} variant="destructive">
-                  <LogOut className="mr-2 h-4 w-4" />
-                  Logout
-                </Button>
-              ) : (
-                <Button variant="secondary">Login</Button>
-              )}
-            </div>
+              </SheetTitle>
+              <SheetDescription className="grid gap-2">
+                {links?.map((link) => (
+                  <Link
+                    key={link?.url}
+                    href={link?.url}
+                    className="capitalize font-bold hover:underline"
+                  >
+                    {link?.label}
+                  </Link>
+                ))}
+                <span className="space-x-3 py-5">
+                  <Button variant={"secondary"}>
+                    <Link href="/login">Login</Link>
+                  </Button>
+                  <Button variant={"outline"}>
+                    <Link href="/signup">Sign Up</Link>
+                  </Button>
+                </span>
+              </SheetDescription>
+            </SheetHeader>
           </SheetContent>
         </Sheet>
-      </div>
-    </nav>
+      </nav>
+    </header>
   );
-};
+}
